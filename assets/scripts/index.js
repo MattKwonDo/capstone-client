441731bba0ba494040f97d69016f3eba3866a2e6
MattKwonDo <mgduvall@gmail.com>
2017-06-05 14:06:39 -0400

    add scripts for authorization to change display
    all auth functions work (sign-in/out/up, change pw) and show / hide
    things appropriately.
    Get beer, create beer entry buttons don't yet work.
    
    	new file:   assets/scripts/auth/api.js
    	new file:   assets/scripts/auth/events.js
    	new file:   assets/scripts/auth/ui.js
    	modified:   assets/scripts/index.js
    	modified:   index.html

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,13 +2,64 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
+const authEvents = require('./auth/events.js')
 
 $(() => {
   setAPIOrigin(location, config)
+  $('.form-control').val('')
+  $('#add-task-modal').on('hidden.bs.modal', function () {
+    $(this).find('input,textarea,select').val('').end()
+  })
+  $('#changePasswordModal').on('hidden.bs.modal', function () {
+    $(this).find('input,textarea,select').val('').end()
+    $('#ChangePasswordError').hide()
+    $('#ChangePasswordSuccess').hide()
+    $('.form-group-pw').show()
+  })
+  $('#signUpModal').on('hidden.bs.modal', function () {
+    $(this).find('input,textarea,select').val('').end()
+    $('#signInError').hide()
+  })
+  $('#signInModal').on('hidden.bs.modal', function () {
+    $(this).find('input,textarea,select').val('').end()
+    $('#signUpError').hide()
+    $('#signUpSuccess').hide()
+  })
+  $('#createBeer').on('hidden.bs.modal', function () {
+    $(this).find('input,textarea,select').val('').end()
+  })
+
+  authEvents.addHandlers()
+  $('.form-control').val('')  // This clears out all the form input fields when the document is first loaded
+  $('#signUpModal').hide()
+  $('.nav-btns').hide()
+  $('.create-a-beer').hide()
+  $('.update-a-beer').hide()
+  $('.get-beers').hide()
 })
 
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
+// query parameters from URL
+// $(document).ready(function () {
+//   $('input:radio').attr('checked', false)
+//   $('input[typed="radio"]').removeAttr('checked')
+// // Check if the URL parameter is apples
+//   const dc = beerEvents.dynamicContent
+//   // console.log(dc)
+//   if (dc[0] === 'respondents' && dc[1] !== undefined) { // Check if the URL parameter is bananas
+//     $('#respondents').show()
+//     api.getOneDynamicBeer(dc[1])
+//       .then(ui.getOneDynamicBeerSuccess)
+//       .catch(ui.getOneDynamicBeerFailure)
+//     // Check if the URL parmeter is empty or not defined, display default content
+//   } else {
+//     $('#default-content').show()
+//   }
+// })
 
-// use require without a reference to ensure a file is bundled
-require('./example')
+$(document).ready(function () {
+  $('#respondents').bind('keypress', function (event) {
+    if (event.keyCode === 13) {
+      return false
+    }
+  })
+})
