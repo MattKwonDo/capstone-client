19b0e33c9941f192558f402672fc251a3a1926da
MattKwonDo <mgduvall@gmail.com>
2017-06-06 14:35:04 -0400

    remove extra "/" from prod URI, update name in
    package.json
    	modified:   assets/scripts/config.js
    	modified:   package.json

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -3,7 +3,7 @@
 const config = {
   apiOrigins: {
     development: 'http://localhost:4741',
-    production: 'https://guarded-refuge-25258.herokuapp.com/'
+    production: 'https://guarded-refuge-25258.herokuapp.com'
   }
 }
 
